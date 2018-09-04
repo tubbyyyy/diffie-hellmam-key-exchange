c4971d4157d224df62a35af6881fab2a861bba81
gurvinder <gurvinder@users.noreply.github.com>
2018-09-04 18:03:26 -0400

    added generatePrime method

--- checkPrimes.py
+++ checkPrimes.py
@@ -1,10 +1,19 @@
 import math
+import random
 
-def isPrime(number):
-    if number < 2:
-        return false
-    castedSqrtOfPrime = int(math.sqrt(number))
-    for i in range(2, castedSqrtOfPrime + 1):
-        if number % i == 0:
-            return false
-    return true
+class Prime:
+    def isPrime(self, number):
+        if number < 2:
+            return False
+        castedSqrtOfPrime = int(math.sqrt(number))
+        for i in range(2, castedSqrtOfPrime + 1):
+            if number % i == 0:
+                return False
+        return True
+
+    def generatePrime(lowerLimit, upperLimit):
+        primeList = []
+        for i in range(lowerLimit, upperLimit):
+            if isPrime(i):
+                primeList.append(i)
+        return primeList[random.randint(0, len(primeList) + 1)]
